40902253a41699874d665229adef909e56499cfe
erick <erichcardosokempner@gmail.com>
2025-12-03 00:06:32 -0300

    m

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -54,10 +54,7 @@
 
                 </div>
                 
-                <NuxtLink to="/"
-                    class="mt-auto block w-full border-2 text-center text-white border-cyan-400 rounded-lg text-sm font-bold p-2 cursor-pointer hover:bg-cyan-400 hover:text-black transition-colors uppercase tracking-wide">
-                    Saiba Mais
-                </NuxtLink>
+                <OpenDetails />
 
             </div>
         </div>
---
+++ app/components/OpenDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtLink :to="actionUrl"
+        class="mt-auto block w-full border-2 text-center text-white border-cyan-400 rounded-lg text-sm font-bold p-2 cursor-pointer hover:bg-cyan-400 hover:text-black transition-colors uppercase tracking-wide">
+        Saiba Mais
+    </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    actionUrl: string
+}>()
+</script>
+
+<style scoped>
+
+</style>
